97ff4f12e4f1d3d9b50ca6c6e11e1a03b8eb074c
gilsonsilva <gilsilvait@gmail.com>
2020-08-25 09:37:36 -0400

    Fisrt commit. Debug exercise.

---
+++ src/Debugging.java
@@ -0,0 +1,14 @@
+import java.util.*;
+//import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+
+
+public class Debugging
+{
+    public static void main (String[] args)
+    {
+        //Block of  code goes here.
+        System.out.println("Mission Accomplished...");
+    }
+}
